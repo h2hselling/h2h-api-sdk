130173044b9e857ad64847f59ca4de429fe8e078
Michael Munson <michael.s.munson@outlook.com>
2026-01-10 19:06:34 -0500

    first commit

---
+++ tsup.config.ts
@@ -0,0 +1,19 @@
+
+
+
+import {defineConfig} from 'tsup';
+
+export default defineConfig({
+    entry: [
+      './src/index.ts',
+      './src/client.ts',
+      './src/api.ts',
+      './src/schema.ts'
+    ],
+    format: ['esm', 'cjs'],
+    dts: true,
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+});
+
